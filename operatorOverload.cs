c706dc85d16271c28eb96241638dcb0ab413291d
TMBMN <77047069+tmbmnadim@users.noreply.github.com>
2022-03-29 23:42:09 +0600

    OperatorOverload added

---
+++ operatorOverload.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace operatorOverload
+{
+    class office{
+        double salary;
+        public office(){
+            salary = 0;
+        }
+
+        public office(double sal){
+            salary=sal;
+        }
+        public void display(){
+            Console.WriteLine(salary);
+        }
+        public static office operator +(office o, office ob){
+            office obj = new office();
+            obj.salary = o.salary+ob.salary;
+            return obj;
+        }
+        static void Main(){
+            office a = new office(1200);
+            office b = new office(7880);
+            office accountant;
+            accountant = a+b;
+            accountant.display();
+        }
+    }
+}
